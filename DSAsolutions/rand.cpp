d40d85d76430a06975faba36fec44b41c9f90085
Mayuresh <mayureshngorantiwar@gmail.com>
2023-01-28 12:55:41 +0530

    k

--- DSAsolutions/rand.cpp
+++ DSAsolutions/rand.cpp
@@ -1,37 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int binarySearch(int arr[], int n , int key)
-{
-    int s=0;
-    int e = n;
-    int mid;
-
-    while(s<=e)
+ 
+//selection sort in arrays
+//find the minimum element in array and swap it to first position
+//then increment index and repeat
+int main()
+{   
+    int arr[5] = {20,10,90,51,6};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<size;i++)
     {
-        mid = s+e/2;
-        if (arr[mid] == key)
+        for(int j = i+1; j<size+1 ; j++)
         {
-            return mid;
-        }
-        else if(arr[mid]<key)
-        {
-            s = mid + 1 ;
-        }
-        else if(arr[mid]>key)
-        {
-            e = mid -1 ;
+            if(arr[j]<arr[i])
+            {
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
         }
     }
-    return -1;
-}
 
-int main()
-{
-    int k;
-    int arr[5] = {15,20,35,60,90};
-    cout<<"Enter number to search";
-    cin>>k;
-    cout<<binarySearch(arr,5,k);
+    for(int k = 0 ; k < size; k++)
+    {
+        cout<<arr[k]<<" ";
+    }
     return 0;
 }
